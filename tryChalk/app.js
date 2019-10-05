aacd05d8f8f0f7e6b13bb9ac975d58d220bfec95
tawnyblvd <margara.sees@gmail.com>
2019-10-05 15:06:44 -0400

    update ex1&2 to include function

--- tryChalk/app.js
+++ tryChalk/app.js
@@ -1,5 +1,20 @@
-const chalk = require("chalk");   //naming it as a variable just in case
+const chalk = require("chalk");   //naming it as a variable
+
+let helloWorld = "Hello world";
 
 //Exercises
 //1. Write a function called `helloBlue` that will console.log the string 'Hello world' in blue.
-console.log(chalk.blue("Hello world"));
+const helloBlue = (str) => {
+  console.log(chalk.blue(str));
+}
+helloBlue(helloWorld);
+
+//2. Write a function called `helloRed` that will console.log the string 'Hello world' in red.
+const helloRed = (str) => {
+  console.log(chalk.red(str));
+}
+helloRed(helloWorld);
+
+//3. Write a function called `stringToColor`. It should take two arguments: a string and a color.
+//The function should log the string to the console in the given color. For example,
+// `stringToColor('this is a test', 'red')` woll log  `"this is a test` in red.
