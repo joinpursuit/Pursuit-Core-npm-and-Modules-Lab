04bdba5eab1d1a5bc3e732ba2395b5d329a95ae8
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-10-05 18:01:34 -0400

    math modules

---
+++ mymodules/main.js
@@ -0,0 +1,17 @@
+const myMath = require("./math-module")
+
+let fourPlusFive = myMath.sum(4, 5)
+
+console.log(fourPlusFive)
+
+let threeTimesFour = myMath.multiply(3, 4)
+
+console.log(threeTimesFour)
+
+let eightDividedBytwo = myMath.divide(8, 2)
+
+console.log(eightDividedBytwo)
+
+let fiveSquared = myMath.square(5)
+
+console.log(fiveSquared)
---
+++ mymodules/math-module.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const square = (n) => {
+    return Math.pow(n, 2);
+}
+
+module.exports = {
+    sum,
+    multiply,
+    divide,
+    square,
+}
