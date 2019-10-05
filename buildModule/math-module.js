3f0a5832db2dadbb04ec9b5ca6c2a91ce5c49e30
vonbarown <vonielbrown@pursuit.org>
2019-10-05 17:31:40 -0400

    building the modules

---
+++ buildModule/math-module.js
@@ -0,0 +1,11 @@
+const sum = (num1, num2) => num1 + num2
+
+const multiply = (num1, num2) => num1 * num2
+
+const square = (num) => Math.pow(num, 2)
+
+module.exports = {
+    sum,
+    multiply,
+    square,
+}
