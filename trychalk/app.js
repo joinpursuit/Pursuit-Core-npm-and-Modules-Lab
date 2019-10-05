1bfbf89384c399e7f1083486bd976c518151fbc1
Hupaul <hupaulcamacho@pursuit.org>
2019-10-05 11:07:30 -0400

    Create app.js file, init npm, and install chalk. Define variables in app.js

---
+++ trychalk/app.js
@@ -0,0 +1,12 @@
+const chalk = require("chalk");
+
+
+const helloBlue = () => {
+    // Hello world in blue
+    console.log("Hello World");
+}
+
+const helloRed = () => {
+    // Hello world in red
+    console.log("Hello World")
+}
