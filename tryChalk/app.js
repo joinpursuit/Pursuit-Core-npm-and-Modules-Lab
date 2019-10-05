99e0e7ffec28b803f208c2c78b5ee523e0f6cc2e
tawnyblvd <margara.sees@gmail.com>
2019-10-05 14:59:54 -0400

    work on exercise one blue hello world

--- tryChalk/app.js
+++ tryChalk/app.js
@@ -1,1 +1,5 @@
-require("chalk");
+const chalk = require("chalk");   //naming it as a variable just in case
+
+//Exercises
+//1. Write a function called `helloBlue` that will console.log the string 'Hello world' in blue.
+console.log(chalk.blue("Hello world"));
