9bf8f2a682a52329e948aba0f71af1efe3b1095a
tawnyblvd <margara.sees@gmail.com>
2019-10-05 17:23:21 -0400

    add results of additional operations

--- main/main.js
+++ main/main.js
@@ -2,3 +2,16 @@ let mathModuleConnection = require("./math-module");
 
 let sumResult = mathModuleConnection.sum(4,6);
 console.log(sumResult);
+//10
+
+let multiplyResult = mathModuleConnection.multiply(5,6);
+console.log(multiplyResult);
+//30
+
+let divideResult = mathModuleConnection.divide(8,4);
+console.log(divideResult);
+//2
+
+let squareResult = mathModuleConnection.square(12);
+console.log(squareResult);
+//144
