c2bf00fb81a643de109cdec584d0e7b42fae3b24
Bpmauge <briahanamauge@pursuit.org>
2019-10-05 16:49:46 -0400

    all part 1 questions have been answered

--- trychalk/app.js
+++ trychalk/app.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 console.log('----- QUESTION 1 -----');
 
 const helloBlue = () => {
-console.log(chalk.blue('Hello world'))
+    console.log(chalk.blue('Hello world'))
 }
 helloBlue();
 
@@ -17,9 +17,9 @@ helloRed();
 console.log('----- QUESTION 3 -----');
 
 const stringToColor = (string, color) => {
-let colors = `${color}`;
-let colored = chalk[colors](string) 
-console.log(colored);
+    let colors = `${color}`;
+    let colored = chalk[colors](string)
+    console.log(colored);
 }
 stringToColor('this is a test', 'green');
 
@@ -28,25 +28,23 @@ console.log('----- QUESTION 4 -----');
 const evensBlueOddsYellow = (string) => {
     let stringSplit = string.split(' ');
     let stringJoin = [];
-    // console.log(stringSplit)
 
-    for (let i = 0; i< stringSplit.length; i++){
-        if(i%2 === 0) {
-          stringJoin[i] = chalk.blue('i')
-        } else{
-        stringJoin[i] = chalk.yellow(stringSplit[i])
+    for (let i = 0; i < stringSplit.length; i++) {
+        if (i % 2 === 0) {
+            let blue = chalk.blue(stringSplit[i]);
+            stringJoin.push(blue)
+        } else {
+            let yellow = chalk.yellow(stringSplit[i]);
+            stringJoin.push(yellow);
+        }
     }
+    console.log(stringJoin.join(' '));
 }
-//   stringSplit.join('');
-    // console.log(stringSplit);
-    // console.log(stringJoin);
-}
-console.log('revisit this question')
 evensBlueOddsYellow('This is a test')
 
 console.log('----- QUESTION 5 -----');
 const angryText = (string) => {
-let capString = string.toUpperCase();
+    let capString = string.toUpperCase();
     console.log(chalk.red.underline.bold(capString))
 }
 angryText('Question 4 was too hard!');
@@ -54,20 +52,26 @@ angryText('Question 4 was too hard!');
 console.log('----- QUESTION 6 -----');
 
 const backgroundCyan = (string) => {
-console.log(chalk.white.bgCyan(string))
+    console.log(chalk.white.bgCyan(string))
 }
-backgroundCyan('What is the background color?')
+backgroundCyan('What is the background color?');
 
 console.log('----- QUESTION 7 -----');
 
 const boldFirstUnderlineLast = (string) => {
-    let array = string.split(' ')
-    let array[0] = chalk.bold(str);
-    let array[array.length-1] = chalk.underline();
-   
+    let array = string.split(' ');
+    let first  = chalk.bold(array[0]);
+    let last  = chalk.underline(array[array.length - 1]);
+    array[0] = first;
+    array[array.length-1] = last;
     console.log(array.join(' '))
 }
 boldFirstUnderlineLast('mic check 1212')
-console.log('revisit this question')
 
 console.log('----- QUESTION 8 -----');
+
+const commandLineChalk = () => {
+    const args = process.argv.slice(2)
+    console.log(chalk.magentaBright(args.join(' ')))
+}
+commandLineChalk();
