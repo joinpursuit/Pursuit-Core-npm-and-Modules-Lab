7e160a7544e1c4483300c47f2ce001f9914162f4
tawnyblvd <margara.sees@gmail.com>
2019-10-05 17:14:51 -0400

    add additional arithmetic functions

--- main/math-module.js
+++ main/math-module.js
@@ -2,7 +2,22 @@ const sum = (num1, num2) => {
   return(num1 + num2);
 }
 
+const multiply = (num1, num2) => {
+  return(num1 * num2);
+}
+
+const divide = (num1, num2) => {
+  return(num1 % num2);
+}
+
+const square = (num) => {
+  return(num * num);
+}
+
 
 module.exports = {
-  sum
+  sum,
+  multiply,
+  divide,
+  square
 }
