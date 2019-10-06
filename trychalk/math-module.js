d008813cebb95fa0f27257e0d68ce5c1080c3342
gsanchez13 <gisellesanchez@pursuit.org>
2019-10-06 00:47:47 -0400

    divide function

--- trychalk/math-module.js
+++ trychalk/math-module.js
@@ -4,6 +4,9 @@ const sum = (num1, num2) => {
 const multiply = (num1, num2) => {
     return num1 * num2;
 }
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
 module.exports = {
     sum,
     multiply
