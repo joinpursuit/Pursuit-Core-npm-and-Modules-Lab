feade300caecfc801f3c0996cbf403d59f6ae2a9
david maravillas <davidmaravillas@pursuit.org>
2020-01-04 17:23:37 -0500

    test

---
+++ part 2/math-module.js
@@ -0,0 +1,8 @@
+const sum = (num1, num2) => num1+num2
+const multiply = (num1, num2) => num1*num2
+const divide = (num1, num2) => num1/num2
+const square = num1 => num1*num1
+
+module.exports{ 
+    sum,multiply,divide,square 
+}
