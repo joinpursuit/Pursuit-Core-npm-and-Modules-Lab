ac0432abc3cd6ade5c00be57d5ffa195f80c430c
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-04 16:22:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+console.log("question 1")
+console.log(chalk.blue("Hello World"))
+console.log("question 2")
+console.log(chalk.red("Hello World"))
+console.log("question 3")
+const stringToColor =()=>{
+    console.log(chalk.red('this is a test')+ " "+ "in red")
+}
+stringToColor()
