5973bcef9ac918712361be91288897996a1b8db4
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-04 17:13:41 -0500

    add more git functions

---
+++ myModule/main.js
@@ -0,0 +1,6 @@
+const {sum,multiply,divide,square} =  require('./math-module.js')
+
+console.log(sum(3, 6))
+console.log(multiply(3, 6))
+console.log(divide(6, 3))
+console.log(square(3))
---
+++ myModule/math-module.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => a + b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+const square = (a) => a*a
+
+module.exports = {
+  sum:sum,
+  multiply,
+  divide,
+  square
+
+}
