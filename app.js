8429e8a66baefe4136540c80fdf199dc6a882208
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-04 17:10:44 -0500

    question 5 done

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(chalk.blue("Hello World"))
 console.log("question 2")
 console.log(chalk.red("Hello World"))
 console.log("question 3")
-const stringToColor =()=>{
+const stringToColor =(str)=>{
     console.log(chalk.red('this is a test')+ " "+ "in red")
 }
 stringToColor()
+
+console.log("question 4")
+const evensBlueOddYellow = (str)=>{
+    let newArr = str.split(" ");
+   
+    let output = "";
+
+    newArr.forEach(el => {
+        if(el % 2 === 0){
+            output += `${chalk.blue(newArr[el])}`
+            
+        } else{
+             output += `${chalk.yellow(newArr[el])}`
+        }
+    });
+    
+    return output
+}
+
+console.log(evensBlueOddYellow("this is a test"))
+console.log("question 5")
+const angryText = (str)=>{
+    let strCap = str.toUpperCase()
+
+
+    console.log(chalk.red.underline.bold(strCap))
+}
+angryText("this is a test")
