6f91cad57c1d4be6b51478588e3c66922e48d953
BrandonBrown <brandonbrown@pursuit.org>
2020-01-04 17:41:08 -0500

    save

---
+++ trychalk/app.js
@@ -0,0 +1,60 @@
+const chalk = require("chalk")
+
+
+const helloBlue = () => {
+    console.log(chalk.blue('hello world'))
+}
+const helloRed = () => {
+    console.log(chalk.red('hello world'))
+}
+// helloRed()
+
+const stringToColor = (string, color) => {
+    console.log(chalk.keyword(color)(string))
+
+}
+// stringToColor("This is a test","red")
+        
+const evensBlueOddsYellow = (string) => {
+let newString =""
+let color = "yellow"
+for(let i = 0; i < string.length; i++){
+    if(string[i] === " " && color === "yellow")  {
+        color = "blue"
+    }else if(string[i] === " " && color === "blue"){
+        color = "yellow"
+    }
+    newString += chalk.keyword(color)(string[i])
+}
+console.log(newString)
+}
+evensBlueOddsYellow("this is a test")
+
+const angryText = (string) => {
+console.log(chalk.red.underline.bold(string.toUpperCase()))
+}
+angryText("im angry")
+
+const backgroundCyan = (string) => {
+    console.log(chalk.bgCyan(string))
+}
+backgroundCyan("hi world")
+
+const boldFirstUnderlineLast = (string) => {
+    // let newString =" "
+    let output = string.split(" ")
+
+    // console.log(output)
+    for(let i = 0; i < output.length; i++){
+        output[0] = chalk.bold(output[0])
+        output[output.length - 1] = chalk.underline(output[output.length - 1])
+    }
+    console.log(output.join(" "))
+
+}
+boldFirstUnderlineLast("how are you doing")
+
+// const commandLineChalk = () => {
+
+// }
+console.log(process.argv)
