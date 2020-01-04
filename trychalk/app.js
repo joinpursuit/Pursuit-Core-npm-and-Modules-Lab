fef1367c0bda63875e1305951ea022a08b2cfafa
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-04 16:00:49 -0500

    add required files

---
+++ trychalk/app.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk")
+
+const helloBlue = () => {
+  console.log(chalk.blue("Hello Blue"));
+}
+
+const helloRed = () => {
+  console.log(chalk.red("Hello Blue"));
+}
+helloBlue()
+helloRed()
