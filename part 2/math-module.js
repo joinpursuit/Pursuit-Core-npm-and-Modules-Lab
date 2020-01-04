49cbf8aa35c231c8ca7bab51c2d6c53cf28e555b
david maravillas <davidmaravillas@pursuit.org>
2020-01-04 17:33:15 -0500

    part 2 complete

--- part 2/math-module.js
+++ part 2/math-module.js
@@ -3,6 +3,9 @@ const multiply = (num1, num2) => num1*num2
 const divide = (num1, num2) => num1/num2
 const square = num1 => num1*num1
 
-module.exports{ 
-    sum,multiply,divide,square 
+module.exports = { 
+    sum,
+    multiply,
+    divide,
+    square 
 }
