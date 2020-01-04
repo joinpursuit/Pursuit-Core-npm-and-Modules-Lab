16695ed2960dd84a2216a65bbdd92dbcf13102c8
Kevin Wong <kevinwong@pursuit.org>
2020-01-04 17:54:30 -0500

    complete part 2 of lab

---
+++ trymath/main.js
@@ -0,0 +1,12 @@
+const math = require('./math-module.js');
+
+let add = math.sum(4,5);
+let mult = math.multiply(2,2);
+let div = math.divide(4,2);
+let square = math.square(4);
+
+console.log(add);
+console.log(mult);
+console.log(div);
+console.log(square);
+
