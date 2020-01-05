c229797ed19a7f595d530f6ad61daae0fb944817
bsihame <sihamebazi@pursuit.org>
2020-01-05 12:17:41 -0500

    solve string module

--- mathMod/main.js
+++ mathMod/main.js
@@ -1,6 +1,9 @@
 const mathMod = require("./math-module.js")
-console.log(mathMod.sum(3, 6))
+const stringsModule = require("./strings-module")
 
+console.log(mathMod.sum(3, 6))
 console.log(mathMod.multiply(2,5))
 console.log(mathMod.divide(20,10))
 console.log(mathMod.square(5))
+
+console.log(stringsModule.firstChar("Hello"))
---
+++ mathMod/strings-module.js
@@ -0,0 +1,8 @@
+let firstChar = (str) => {
+ return str.firstChar(0)
+}
+
+module.exports = {
+  firstChar,
+}
+
