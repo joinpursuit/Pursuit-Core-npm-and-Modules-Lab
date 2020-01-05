b7b5ec27997b2cfd7f2590832fc356657d976210
bsihame <sihamebazi@pursuit.org>
2020-01-05 12:58:04 -0500

    finish string

--- mathMod/main.js
+++ mathMod/main.js
@@ -7,3 +7,5 @@ console.log(mathMod.divide(20,10))
 console.log(mathMod.square(5))
 
 console.log(stringsModule.firstChar("Hello"))
+console.log(stringsModule.compare("Deja", "Sihame"))
+console.log(stringsModule.reverse("reverse"))
--- mathMod/strings-module.js
+++ mathMod/strings-module.js
@@ -2,7 +2,25 @@ const firstChar = (str) => {
  return str[0]
 }
 
+const compare = (strA,strB) => {
+  if(strA === strB){
+    return true
+  } else {
+    return false
+  }
+}
+
+const reverse = (str) => {
+  let newStr = "";
+  for (let i =str.length-1 ; i >= 0 ; i--){
+    newStr +=str[i]
+  }
+  return newStr
+}
+
 module.exports = {
-  firstChar
+  firstChar,
+  compare,
+  reverse,
 }
 
