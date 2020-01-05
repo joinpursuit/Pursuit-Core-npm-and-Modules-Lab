ac712bf71b611945fee9f986cc4440879926b131
Jay Fowler <jcafowler@gmail.com>
2020-01-05 12:42:53 -0500

    Complete Q4

--- trychalk/app.js
+++ trychalk/app.js
@@ -16,3 +16,11 @@ const stringToColor = (str, color) => {
 
 stringToColor("Halloween", "orange")
 
+const evensBlueOddsYellow = (str) => {
+  let words = str.split(" ")
+  for (let i = 0; i < words.length; i++){
+    i % 2 ? words[i] = chalk.yellow(words[i]) : words[i] = chalk.blue(words[i])
+  }
+  console.log(words.join(" "))
+}
+evensBlueOddsYellow("this is a test")
