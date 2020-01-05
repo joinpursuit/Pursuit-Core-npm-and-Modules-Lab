d132524069db2ad8245b7bc146415fb2b516689a
maria liza <mariamartinez@pursuit.org>
2020-01-05 12:59:22 -0500

    attempt bonus

--- bonus/filterFiles.js
+++ bonus/filterFiles.js
@@ -1,28 +1,25 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
 
 // const folder = process.argv[2]
 // const ext = '.' + process.argv[3]
 
-const filterFilesFn = (fName,ext, fn) =>{
-    let dExt = []
-    
-    fs.readdir(folder, (err, files) => {
-        if (err) {
-            fn(err);
-            return;
-        }
-        
-        files.filter((file) => {
-            if (path.extname(file) === ext) {
-                dExt.push(file)
-            }
-        })
+const filterFilesFn = (fName, ext, fn) => {
+  let dExt = []
 
-        fn("null", dExt)
+  fs.readdir(fName, (err, files) => {
+    if (err) {
+      return fn(err)
+    }
+
+    files.filter(file => {
+      if (path.extname(file) === ext) {
+        dExt.push(file)
+      }
     })
+
+    fn("null", dExt)
+  })
 }
 
-module.exports = {
-    filterFilesFn
-}
+module.exports = { filterFilesFn }
